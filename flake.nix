6b3bd55ffc82fee43eeefd825645470842567fb0
Milutin Popovic <milutin@popovic.xyz>
2026-09-05 23:08:58 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "dmenu-mac development shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.gnumake ];
+
+            shellHook = ''
+              if ! xcode-select -p >/dev/null 2>&1; then
+                echo "warning: Xcode Command Line Tools are not installed"
+              fi
+            '';
+          };
+        });
+    };
+}
